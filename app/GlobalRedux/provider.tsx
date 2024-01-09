a1d21c08f506020e160d61b400ee13b3393a0102
ashkan <ashkanpc2003@gmail.com>
2024-01-09 18:48:09 +0330

    fix the first-render issue of the redux-tooklit

--- app/GlobalRedux/provider.tsx
+++ app/GlobalRedux/provider.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { AppStore, store } from "./store";
+import { useRef } from "react";
 
 export function ReduxProvider({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  const storeRef = useRef<AppStore>();
+  if (!storeRef.current) {
+    // Create the store instance the first time this renders
+    storeRef.current = store();
+  }
+  return <Provider store={storeRef.current}>{children}</Provider>;
 }
--- app/GlobalRedux/store.ts
+++ app/GlobalRedux/store.ts
@@ -2,16 +2,19 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./Features/auth/auth-slice";
-import darkModeReducer from "./Features/darkMode/darkMode-slice"
+import darkModeReducer from "./Features/darkMode/darkMode-slice";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
 
-export const store = configureStore({
-  reducer: {
-    authReducer,
-    darkModeReducer
-  },
-});
+export const store = () => {
+  return configureStore({
+    reducer: {
+      authReducer,
+      darkModeReducer,
+    },
+  });
+};
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type AppStore = ReturnType<typeof store>;
 export const useAppSelectore: TypedUseSelectorHook<RootState> = useSelector;
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
